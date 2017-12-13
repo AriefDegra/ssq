c7956ffdc6f4cdd886f6c36a2c189b7be990eabe
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2017-12-14 05:51:45 +0600

    Problem fixed

--- src/ssq/EventList.java
+++ src/ssq/EventList.java
@@ -18,6 +18,7 @@ class EventList extends LinkedList {
     }
 
     public Object getMin() {
+        sort();
         return getFirst();
     }
 
@@ -27,5 +28,15 @@ class EventList extends LinkedList {
 
     public void dequeue() {
         removeFirst();
+    }
+
+
+    public void sort(){
+        Collections.sort(this,  new Comparator<Event>() {
+            @Override public int compare(Event e1, Event e2) {
+                return e1.getTime() > e2.getTime() ? 1 : (e1.getTime() < e2.getTime() ? -1 : 0);
+            }
+
+        });
     }
 }
